26faf9c679c9e1cbda39ccde2b0431d7be1e08ca
thomaslaber <tlaber@student.tgn>
2024-09-25 18:56:12 +0200

    wrote javadoc comments for the Controller class

--- src/main/java/at/tlaber/controller/Controller.java
+++ src/main/java/at/tlaber/controller/Controller.java
@@ -8,15 +8,27 @@ import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 
+/**
+ * Controller class
+ * @author Thomas Laber
+ * @version 2024-09-25
+ */
 public class Controller {
     private WordTrainer worttrainer;
     private Frame frame;
     private Panel panel;
 
+    /**
+     * main methode for Controller
+     * @param args
+     */
     public static void main(String[] args) {
         new Controller();
     }
 
+    /**
+     * Controller constructor
+     */
     public Controller() {
         this.worttrainer = new WordTrainer();
         try {
@@ -27,18 +39,34 @@ public class Controller {
         this.frame = new Frame(this.panel);
     }
 
+    /**
+     * getter Methode for Url
+     * @return the Url of the currently displayed Word
+     */
     public String getUrl() {
         return this.worttrainer.getCurrent().getUrl();
     }
 
+    /**
+     * getter Methode for correct
+     * @return how many correct answers the user has given
+     */
     public int getCorrect() {
         return this.worttrainer.getCorrect();
     }
 
+    /**
+     * getter methode for total
+     * @return how many total answers the user has given
+     */
     public int getTotal() {
         return this.worttrainer.getTotal();
     }
 
+    /**
+     * the methode which reacts to the actions the user takes in the program
+     * @param action the action the user did
+     */
     public void performAction(ActionEvent action) {
         String actionCommand = action.getActionCommand();
         switch (actionCommand) {
